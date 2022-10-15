0d9ee25eb7ed824f2c840973db0a7b32f3bb6e0e
SoCuul <socuul@outlook.com>
2022-10-14 20:45:17 -0700

    Delete messages that do not include a prefix in selling channels

--- Bot/events/messageCreate.js
+++ Bot/events/messageCreate.js
@@ -5,6 +5,22 @@ export const name = 'messageCreate'
 
 export const execute = async (client, message) => {
     try {
+        //Check if message is in a buy/trade/sell channel
+        if (message?.channel?.name?.toLowerCase()?.includes('buy-trade-sell')) {
+            //Check if message includes required prefix
+            if (!message?.content?.toLowerCase()?.startsWith('wtb') && !message?.content?.toLowerCase()?.startsWith('wtt') && !message?.content?.toLowerCase()?.startsWith('wts')) {
+                //Delete message
+                try {
+                    await message.delete()
+                }
+                catch (error) {
+                    console.log(error)
+                }
+
+                return
+            }
+        }
+
         //Respond to pitch black msgs
         if (message?.content?.toLowerCase()?.match('((tastes|taste|tasted) like ((pitch black)|pb|(mtn dew pitch black)|(mtn dew pb)|(mountain dew pitch black)|(mountain dew pb)))')) {
             await message.react('997376586800168990')
